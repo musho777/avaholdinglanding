8c6367dbef0830dc411d015fd41670d2bdc107da
musho7-dev <129147695+musho777@users.noreply.github.com>
2026-07-30 15:08:30 +0400

    bug fix

--- app/(home)/components/YerevanLocation/YerevanLocation.tsx
+++ app/(home)/components/YerevanLocation/YerevanLocation.tsx
@@ -8,14 +8,11 @@ export default function YerevanLocation() {
       <div className="map-wrapper">
         <img className="map-image" src="/assets/AVA_Map.svg" alt="Yerevan map" />
 
-        {/* Text overlay on the left */}
         <div className="map-text-overlay">
           <h2 className="map-title">
             <span className="title-line scroll-reveal">PRIME LOCATION</span>
-            <span className="title-small scroll-reveal delay-1">IN THE HEART OF</span>
-            <span className="title-line scroll-reveal delay-1">YEREVAN,</span>
-            <span className="title-small scroll-reveal delay-2">CLOSE TO</span>
-            <span className="title-line scroll-reveal delay-2">HISTORIC SITES</span>
+            <span className="title-line scroll-reveal delay-1">IN THE HEART OF</span>
+            <span className="title-line scroll-reveal delay-1">YEREVAN</span>
           </h2>
           <button className="see-map-btn scroll-reveal delay-3">SEE ON MAP</button>
         </div>

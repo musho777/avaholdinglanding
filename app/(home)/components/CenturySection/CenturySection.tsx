6fc2276954324018f89c2a8d2a0e1e47b66976fa
musho7-dev <129147695+musho777@users.noreply.github.com>
2026-08-03 21:16:42 +0400

    bug fix

--- app/(home)/components/CenturySection/CenturySection.tsx
+++ app/(home)/components/CenturySection/CenturySection.tsx
@@ -15,24 +15,48 @@ export default function CenturySection() {
           // When section is leaving viewport (scrolling down past it)
           if (!entry.isIntersecting && entry.boundingClientRect.top < 0) {
             setHasExited(true);
+            if (sectionRef.current) {
+              sectionRef.current.classList.add("century-scrolled");
+            }
             if (topTextRef.current) {
-              topTextRef.current.classList.remove("scroll-reveal", "scroll-revealed", "delay-1", "century-enter-from-left");
+              topTextRef.current.classList.remove(
+                "scroll-reveal",
+                "scroll-revealed",
+                "delay-1",
+                "century-enter-from-left"
+              );
               topTextRef.current.classList.add("century-exit-left");
             }
             if (bottomTextRef.current) {
-              bottomTextRef.current.classList.remove("scroll-reveal", "scroll-revealed", "delay-2", "century-enter-from-right");
+              bottomTextRef.current.classList.remove(
+                "scroll-reveal",
+                "scroll-revealed",
+                "delay-2",
+                "century-enter-from-right"
+              );
               bottomTextRef.current.classList.add("century-exit-right");
             }
           } else if (entry.isIntersecting) {
             // When section is in view
+            if (sectionRef.current) {
+              sectionRef.current.classList.remove("century-scrolled");
+            }
             if (hasExited) {
               // Coming back from below - slide from left/right to center
               if (topTextRef.current) {
-                topTextRef.current.classList.remove("century-exit-left", "scroll-reveal", "delay-1");
+                topTextRef.current.classList.remove(
+                  "century-exit-left",
+                  "scroll-reveal",
+                  "delay-1"
+                );
                 topTextRef.current.classList.add("century-enter-from-left");
               }
               if (bottomTextRef.current) {
-                bottomTextRef.current.classList.remove("century-exit-right", "scroll-reveal", "delay-2");
+                bottomTextRef.current.classList.remove(
+                  "century-exit-right",
+                  "scroll-reveal",
+                  "delay-2"
+                );
                 bottomTextRef.current.classList.add("century-enter-from-right");
               }
             } else {
@@ -42,7 +66,10 @@ export default function CenturySection() {
                 topTextRef.current.classList.add("scroll-reveal", "delay-1");
               }
               if (bottomTextRef.current) {
-                bottomTextRef.current.classList.remove("century-exit-right", "century-enter-from-right");
+                bottomTextRef.current.classList.remove(
+                  "century-exit-right",
+                  "century-enter-from-right"
+                );
                 bottomTextRef.current.classList.add("scroll-reveal", "delay-2");
               }
             }
@@ -51,7 +78,7 @@ export default function CenturySection() {
       },
       {
         threshold: [0.9, 1],
-        rootMargin: "-20px 0px 0px 0px",
+        rootMargin: "-5px 0px 0px 0px",
       }
     );
 
@@ -78,7 +105,11 @@ export default function CenturySection() {
       </div>
       <div className="century-scroll-indicator">
         <svg xmlns="http://www.w3.org/2000/svg" width="10" height="72.5" viewBox="0 0 10 72.5">
-          <path d="M17.5,0l-.909.909,3.442,3.442H-50v1.3H20.032L16.591,9.091,17.5,10l5-5Z" transform="translate(10 50) rotate(90)" fill="#fff"></path>
+          <path
+            d="M17.5,0l-.909.909,3.442,3.442H-50v1.3H20.032L16.591,9.091,17.5,10l5-5Z"
+            transform="translate(10 50) rotate(90)"
+            fill="#fff"
+          ></path>
         </svg>
       </div>
     </section>
